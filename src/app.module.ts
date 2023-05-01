5a9aeeb22396eda47d43f44589d9533f14657e3d
sungjinWi <0202wsj@gmail.com>
2023-05-01 17:02:56 +0900

    Feat : middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,30 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { AdminController } from './admin/admin.controller';
-import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware, logger } from './common/middleware/logger.middleware';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController, AdminController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      // .exclude(
+      //   { path: 'cats', method: RequestMethod.GET },
+      //   { path: 'cats', method: RequestMethod.POST },
+      //   'cats/(.*)',
+      // )
+      .forRoutes(CatsController) //routes defined inside CatsController
+      // .forRoutes({ path: 'cats', method: RequestMethod.GET})
+
+      // multiple function middleware
+      // .apply(cors(), helmet(),logger)
+      // .forRoutes(CatsController)
+  }
+}
